d1219e0bdd5120c08f4a38fc9f97257d0c6d3adf
Maksim Maksimov <maxamaxim@gmail.com>
2020-05-12 09:48:16 +0300

    2nd

--- hello.py
+++ hello.py
@@ -5,3 +5,21 @@ app = Flask(__name__)
 def hello_world():
     print('test message')
     return '<h1>Hello, YoYoYo!</h1>'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+
+def mean(numbers):
+    return  sum(numbers) / len(numbers) 
+
+
+@app.route('/avg/<nums>')
+def avg(nums):
+    nums = nums.split(',')
+    nums =[int(num) for num in nums]
+    nums_mean = mean(nums)
+    print(nums_mean)
+    return str(nums_mean)
